6224bd96d6ce01be9c5790d750595b9b8fc03d0f
“podliyPadonak” <supercell0boombeach@gmail.com>
2023-12-16 17:08:54 +0200

    solution

---
+++ ZinkovskiyTask/DictionaryOperations.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZinkovskiyTask
+{
+    internal static class DictionaryOperations
+    {
+        internal static void AddKeyOrUpdate(ref SortedDictionary<string, int> dict, ref string item, int count)
+        {
+            if (dict.ContainsKey(item))
+            {
+                dict[item] += count;
+            }
+            else
+            {
+                dict.Add(item, count);
+            }
+        }
+        internal static void AddKeyOrUpdate(ref SortedDictionary<char, int> dict, char item, int count)
+        {
+            if (dict.ContainsKey(item))
+            {
+                dict[item] += count;
+            }
+            else
+            {
+                dict.Add(item, count);
+            }
+        }
+    }
+}
---
+++ ZinkovskiyTask/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ZinkovskiyTask;
+public class Programm
+{
+    public static void Main()
+    {
+        DateTime timeStart = DateTime.Now;
+        var path = "text.txt";
+        var dic = new SortedDictionary<string, int>();
+        var signDic = new SortedDictionary<char, int>();
+        char[] signs = new char[] { '.', ';', '!', '?',':','(',')', '…', '»', '«', ',' };
+        string[] buffer;
+        try
+        {
+            StreamReader sr = new StreamReader(new FileStream(path, FileMode.Open));
+            while (!sr.EndOfStream)
+            {
+                buffer = sr.ReadLine().ToLower().Split(' ');
+                foreach (var it in buffer)
+                {
+                    string item = it;
+                    foreach (var s in signs)
+                    {
+                        int signCount = it.Where(i => i == s).Count();
+                        if (signCount > 0)
+                        {
+                            DictionaryOperations.AddKeyOrUpdate(ref signDic, s, signCount);
+                            item = item.Replace(s.ToString(), string.Empty);
+                        }
+                    }
+                    if (item == "")
+                    {
+                        continue;
+                    }
+                    if (item == "–" || item == "-")
+                    {
+                        DictionaryOperations.AddKeyOrUpdate(ref signDic, '-', 1);
+                    }
+                    else
+                    {
+                        DictionaryOperations.AddKeyOrUpdate(ref dic, ref item, 1);
+                    }
+                }
+            }
+            sr.Close();
+            Console.OutputEncoding = Encoding.UTF8;
+            // LINQ-запрос
+            foreach (var it in dic)
+            {
+                Console.WriteLine("{0}: {1}", it.Key, it.Value);
+            }
+            Console.WriteLine("\nРозділові знаки:");
+            foreach (var it in signDic)
+            {
+                Console.WriteLine("{0}: {1}", it.Key, it.Value);
+            }
+            DateTime timeEnd = DateTime.Now;
+            Console.WriteLine($"\nВитрачено часу: {(timeEnd-timeStart).TotalSeconds} секунд");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        Console.ReadKey();
+    }
+}
